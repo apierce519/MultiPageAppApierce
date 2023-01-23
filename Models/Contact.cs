05749dcd69e63b17f255462bdebdcf283d5cbb12
Andrew Pierce <ajpierce1@dmacc.edu>
2023-01-22 20:13:44 -0600

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiPageAppApierce.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter a Phone Number.")]
+        public int? PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Please enter an Address.")]
+        public string? Address { get; set; }
+        public string? Note { get; set; }
+
+        public string? Slug => Name?.ToLower().ToString();
+    }
+}
