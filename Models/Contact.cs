a53a7f9d258f9ed90f6698429e6141f5a0495195
Andrew Pierce <ajpierce1@dmacc.edu>
2023-01-22 20:52:42 -0600

    Created contact portions

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -7,7 +7,7 @@ namespace MultiPageAppApierce.Models
         [Required(ErrorMessage = "Please enter a name.")]
         public string? Name { get; set; }
         [Required(ErrorMessage = "Please enter a Phone Number.")]
-        public int? PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         [Required(ErrorMessage = "Please enter an Address.")]
         public string? Address { get; set; }
         public string? Note { get; set; }
---
+++ Models/ContactContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MultiPageAppApierce.Models
+{
+    public class ContactContext : DbContext
+    {
+
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact
+                {
+                    Name = "Jack Burton",
+                    PhoneNumber = "5558675309",
+                    Address = "Visalia, California",
+                    Note = "Everybody relax, I'm here."
+                },
+                new Contact
+                {
+                    Name = "Ellen Ripley",
+                    PhoneNumber = "9035768555",
+                    Address = "Olympia Colony, Luna",
+                    Note = "This is commercial towing vehicle Nostromo out of the Solomons, registration number 1-8-0-niner-2-4-6-0-niner."
+                },
+                new Contact
+                {
+                    Name = "Korben Dallas",
+                    PhoneNumber = "C4765536",
+                    Address = "West Park Alleys 281 Level 21 / PIER 23-28 HA CON 37 / 144E NY, NY 10024",
+                    Note = "Negative, I am a meat popsicle."
+                }
+
+            );
+        }
+    }
+}
+
