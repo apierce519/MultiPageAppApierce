7e434cc9a13cec60d72484fcae815a9cd51984aa
Andrew Pierce <ajpierce1@dmacc.edu>
2023-03-22 23:25:50 -0500

    Module 10 assignments

---
+++ Models/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace MultiPageAppApierce.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a description.")]
+        public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter a sprint number.")]
+        public int SprintNumber { get; set; }
+        [Required(ErrorMessage = "Please enter a point value.")]
+        public int PointValue { get; set; }
+        public string StatusId { get; set; } = null!;
+        [Required(ErrorMessage = "Please enter a status.")]
+        public Status Status { get; set; } = null!;
+
+    }
+}
---
+++ Models/TicketContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiPageAppApierce.Models;
+
+namespace MultiPageAppApierce.Models
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options) : base(options) { }
+
+        public DbSet<Ticket> Tickets { get; set; } = null!;
+        public DbSet<Status> Statuses { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId = "inprog", Name = "In Progress" },
+                new Status { StatusId = "qa", Name = "Quality Assurance" },
+                new Status { StatusId = "done", Name = "Done" },
+                new Status { StatusId = "todo", Name = "Todo" }
+
+                );
+        }
+    }
+}
